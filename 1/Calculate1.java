e5f8c7a9155f74fa2e66a42f512322e035b90856
jade55 <jade555@mail.ru>
2015-06-26 14:38:16 +0500

    Добавил сложение

--- 1/Calculate1.java
+++ 1/Calculate1.java
@@ -1,6 +1,10 @@
 class Calculate1{
 	public static void main(String[] arg){
 		System.out.println("Hello world!!!");
+		int first = Integer.valueOf(arg[0]);
+		int second  = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		System.out.println("Sum = " + sum)
 
 	}
 
